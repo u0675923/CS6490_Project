96bbd88c858ff5083e0189f2689ff1762be2a601
Adrian_Flores <u0675923@umail.utah.edu>
2023-04-16 10:50:06 -0600

    finished program timer

--- SpamScannerApp/application.py
+++ SpamScannerApp/application.py
@@ -1,11 +1,13 @@
-import tkinter as tk
-
 from kivy.app import App
 from kivy.core.window import Window
 from kivy.config import Config
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen, ScreenManager
+from kivy.clock import Clock
+
+import time
+import threading
 
 class MainScreen(Screen):
     # images used in program:
@@ -17,26 +19,45 @@ class MainScreen(Screen):
     # indicator for running program
     program_running = False
 
-    Config.set('graphics', 'width', '360')
-    Config.set('graphics', 'height', '640')
-
-    Window.size = (360,640)
-    Window.clearcolor = (52/255,57/255,68/255,1) # RGBA value
-
     def StatusButtonPressed(self):
         self.ids.status_image.source = self.pressed_status
 
     def StatusButtonReleased(self):
         if not self.program_running:
             self.program_running = True
+
+            # start program timer
+            self.start = time.time()
+            self.elapsed_time = 0
+            Clock.schedule_interval(self.UpdateTimeLabel, 1)
+
+            # update button image
             self.ids.status_image.source = self.on_status
         else:
             self.program_running = False
+            
+            Clock.unschedule(self.UpdateTimeLabel)
+            self.elapsed_time = 0
+            self.ids.box.ids.program_runtime.data = "00d 00m 00s"
+
+            # update button image
             self.ids.status_image.source = self.off_status
+    
+    def UpdateTimeLabel(self, dt):        
+        elapsed_time = time.time() - self.start
+        days, r = divmod(elapsed_time, 86400)
+        hours, r = divmod(r, 3600)
+        min, sec = divmod(r, 60)
+        self.ids.box.ids.program_runtime.data = f"{int(days):02d}d {int(min):02d}m {int(sec):02d}s"
 
 class MyApp(App):
     def build(self):
         Builder.load_file('application.kv')
+        Config.set('graphics', 'width', '360')
+        Config.set('graphics', 'height', '640')
+
+        Window.size = (360,640)
+        Window.clearcolor = (52/255,57/255,68/255,1) # RGBA value
         
         sm = ScreenManager()
 
