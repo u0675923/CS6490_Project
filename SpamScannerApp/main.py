78ce4eff146a47e430886b6a677ee0eab84f1e6f
Adrian_Flores <u0675923@umail.utah.edu>
2023-05-01 09:40:21 -0600

    removed unnecessary ui elements

--- SpamScannerApp/main.py
+++ SpamScannerApp/main.py
@@ -7,8 +7,6 @@ from kivy.core.text import LabelBase
 from kivy.config import Config
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
-from kivy.uix.screenmanager import Screen, ScreenManager
-from kivy.clock import Clock
 from android.permissions import request_permissions, Permission
 from jnius import autoclass, cast
 from kivy.utils import platform
@@ -38,49 +36,16 @@ class MainScreen(Screen):
 
             # Start the service
             spam_scanner_service.start(PythonActivity.mActivity, "")
-
-            # start program timer
-            self.start = time.time()
-            self.elapsed_time = 0
-            Clock.schedule_interval(self.UpdateTimeLabel, 1)
-
-            # update button image
-            self.ids.status_image.source = self.on_status
         else:
             self.program_running = False
 
             # Stop the service
             spam_scanner_service.stop(PythonActivity.mActivity)
-            
-            # stop program timer
-            Clock.unschedule(self.UpdateTimeLabel)
-            self.elapsed_time = 0
-            self.ids.box.ids.program_runtime.data = "00m 00s"
 
-            # update button image
-            self.ids.status_image.source = self.off_status
     
     # Buttons for switching screens
     def AboutButtonPressed(self):        
         self.manager.current = 'about_screen'
-    
-    def SettingsButtonPressed(self):        
-        self.manager.current = 'settings_screen'
-
-    def ActivityButtonPressed(self):        
-        self.manager.current = 'activity_screen'
-
-    """
-    Updates the timer label 
-    """
-    def UpdateTimeLabel(self, dt):        
-        elapsed_time = time.time() - self.start
-
-        # For now, just show minutes and seconds
-        #days, r = divmod(elapsed_time, 86400)
-        #hours, r = divmod(r, 3600)
-        min, sec = divmod(elapsed_time, 60)
-        self.ids.box.ids.program_runtime.data = f"{int(min):02d}m {int(sec):02d}s"
 
 class AboutScreen(Screen):
     back_button = 'Images/back_button.png'
@@ -92,37 +57,6 @@ class AboutScreen(Screen):
     def BackButtonReleased(self):
         self.ids.back_button.source = self.back_button
         self.manager.current = 'main_screen'
-
-
-class SettingsScreen(Screen):
-    back_button = 'Images/back_button.png'
-    back_button_pressed = 'Images/pressed_back_button.png'
-
-    def BackButtonPressed(self):
-        self.ids.back_button.source = self.back_button_pressed
-
-    def BackButtonReleased(self):
-        self.ids.back_button.source = self.back_button
-        self.manager.current = 'main_screen'
-
-class ActivityScreen(Screen):
-    contentResolver = PythonActivity.mActivity.getContentResolver()
-
-    back_button = 'Images/back_button.png'
-    back_button_pressed = 'Images/pressed_back_button.png'
-
-    def BackButtonPressed(self):
-        self.ids.back_button.source = self.back_button_pressed
-
-    def BackButtonReleased(self):
-        self.ids.back_button.source = self.back_button
-        self.manager.current = 'main_screen'
-
-    # TODO: Remove this
-    def Do(self):
-        pass
-        # puts the ouput of the messages into the label in activity.kv
-        # tokens = ['testing', "You're Netflix account has been compromised! Please click the link and enter your password to save your account: https//www.test.co", "Hey, this is Kat, how you doing today??", "You could save a BUNDLE with testingNonsense car insurance today!"]
 
 
 class MyApp(App):
@@ -141,8 +75,6 @@ class MyApp(App):
 
         # load the kivy design files
         Builder.load_file('main_screen.kv')
-        Builder.load_file('activity.kv')
-        Builder.load_file('settings.kv')
         Builder.load_file('about.kv')
 
         Window.clearcolor = (52/255,57/255,68/255,1) # RGBA value
@@ -152,14 +84,10 @@ class MyApp(App):
         # create screens for the application
         main_screen = MainScreen(name='main_screen')
         about_screen = AboutScreen(name='about_screen')
-        settings_screen = SettingsScreen(name='settings_screen')
-        activity_screen = ActivityScreen(name='activity_screen')
 
         # add screens to the screen manager
         sm.add_widget(main_screen)
         sm.add_widget(about_screen)
-        sm.add_widget(settings_screen)
-        sm.add_widget(activity_screen)
 
         return sm
 
